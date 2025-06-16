3452bd10154430a98159efb72afdba49dc4671a6
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-06-16 13:45:36 +0530

    basic page

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+        <h2>Welcome to VoiRec, the Smart Hiring Assistant</h2>
+        <Button>Click Me</Button>
+    </div>
+  );
+}
