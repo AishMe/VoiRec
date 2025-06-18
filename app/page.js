3a35187d8eb7bf412ca2840250ef5eca19227fb6
Aishwarya <129073332+AishMe@users.noreply.github.com>
2025-06-18 22:19:35 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
         <h2>Welcome to VoiRec, the Smart Hiring Assistant</h2>
-        <Button>Click Me</Button>
+        <Button variant="ghost">Click Me</Button>
     </div>
   );
 }
